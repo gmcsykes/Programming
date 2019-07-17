271b90776c1be054befc553830af017a4407d101
Gregory Sykes <billymakkarom@gmail.com>
2019-07-17 11:21:30 +0700

    added training file
    You tube learn Python video

---
+++ hello_world_extended.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+phrase = "Nearly Test"
+print ("hello world")
+print ("testing 1,2,3")
+print (phrase.upper().isupper())
+print (len(phrase))
+print (phrase[3])
+print (phrase.index("Test"))
+print (phrase.replace("Test","Rubish"))
+print ("really long test")
+print (phrase.index(rly))
